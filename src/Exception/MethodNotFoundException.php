f6e14a90061cec47fb54dfe70659b44337b39fb8
sy-records <52o@qq52o.cn>
2019-11-04 11:49:39 +0800

    :art: update exception

--- src/Exception/MethodNotFoundException.php
+++ src/Exception/MethodNotFoundException.php
@@ -7,11 +7,9 @@
 
 namespace Luffy\AliHbaseThrift\Exception;
 
-use Throwable;
-
 class MethodNotFoundException extends \Exception
 {
-    public function __construct($message = "", $code = 0, Throwable $previous = null)
+    public function __construct($message = "", $code = 0, \Throwable $previous = null)
     {
         parent::__construct($message, $code, $previous);
     }
