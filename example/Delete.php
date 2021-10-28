fdf38799c763210ebb60ecf8c2f4c170fe335a2a
sy-records <52o@qq52o.cn>
2021-10-28 14:42:50 +0800

    Apply fixes from php-cs-fixer

--- example/Delete.php
+++ example/Delete.php
@@ -5,9 +5,10 @@ declare(strict_types=1);
  * This file is part of thrift2-hbase.
  *
  * @link     https://github.com/sy-records/thrift2-hbase
- * @document https://github.com/sy-records/thrift2-hbase
- * @contact  52o@qq52o.cn
- * @license  https://github.com/sy-records/thrift2-hbase/blob/master/LICENSE
+ * @contact  Lu Fei <52o@qq52o.cn>
+ *
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
  */
 namespace Lufyy\HbaseExample;
 
