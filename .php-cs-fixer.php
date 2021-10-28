35a085c137c49b47969bd86baa91436939c27378
sy-records <52o@qq52o.cn>
2021-10-28 14:39:34 +0800

    Upgraded friendsofphp/php-cs-fixer to ^3.0

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -4,12 +4,13 @@ $header = <<<'EOF'
 This file is part of thrift2-hbase.
 
 @link     https://github.com/sy-records/thrift2-hbase
-@document https://github.com/sy-records/thrift2-hbase
-@contact  52o@qq52o.cn
-@license  https://github.com/sy-records/thrift2-hbase/blob/master/LICENSE
+@contact  Lu Fei <52o@qq52o.cn>
+
+For the full copyright and license information,
+please view the LICENSE file that was distributed with this source code.
 EOF;
 
-return PhpCsFixer\Config::create()
+return (new PhpCsFixer\Config())
     ->setRiskyAllowed(true)
     ->setRules([
         '@PSR2' => true,
@@ -17,7 +18,7 @@ return PhpCsFixer\Config::create()
         '@DoctrineAnnotation' => true,
         '@PhpCsFixer' => true,
         'header_comment' => [
-            'commentType' => 'PHPDoc',
+            'comment_type' => 'PHPDoc',
             'header' => $header,
             'separate' => 'none',
             'location' => 'after_declare_strict',
